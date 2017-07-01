b90aa8e6869e12ac3594a6efca5c334b1b71aae4
Barry Hylton <blhylton@blhylton.com>
2017-07-01 02:52:27 -0400

    Feat: QoL Improvement if no books on BookShelf

--- src/Components/bookShelf.js
+++ src/Components/bookShelf.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types'
 class BookShelf extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
-    books: PropTypes.array
+    books: PropTypes.array.isRequired
   }
 
   render() {
@@ -16,20 +16,23 @@ class BookShelf extends Component {
 
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{name}</h2>
+        <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {books.map((book) => {
-              return (
-                <li key={book.id}>
-                  <BookEntry
-                    thumbnail={book.imageLinks.thumbnail}
-                    title={book.title}
-                    authors={book.authors}
-                  />
-                </li>
-              )}
-            )}
+            {books.length > 0 ? 
+              books.map((book) => {
+                return (
+                  <li key={book.id}>
+                    <BookEntry
+                      thumbnail={book.imageLinks.thumbnail}
+                      title={book.title}
+                      authors={book.authors}
+                    />
+                  </li>
+              )})
+              :
+              (<span>No books on this shelf.</span>)
+              }
           </ol>
         </div>
       </div>
