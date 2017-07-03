a7119d9995bd257c30e9612f766a7909fad55e4e
bhylton <bhylton@stellarstudios.com>
2017-07-03 10:08:12 -0400

    Refactor: Change SearchBar to stateless function

--- src/Components/searchBar.js
+++ src/Components/searchBar.js
@@ -3,38 +3,32 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-class SearchBar extends Component {
-  static propTypes = {
-    onQuery: PropTypes.func.isRequired
-  }
-
-  queryBooks = (e) => {
-    this.props.onQuery(e.target.value)
-  }
-
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link
-            className="close-search"
-            to="/"
-          >Close</Link>
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              onChange={this.queryBooks}
-              value={this.props.query}
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
+const SearchBar = (props) => {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link
+          className="close-search"
+          to="/"
+        >Close</Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            onChange={(e) => props.onQuery(e.target.value)}
+            value={props.query}
+          />
         </div>
       </div>
-    )
-  }
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  )
+}
+
+SearchBar.propTypes = {
+  onQuery: PropTypes.func.isRequired
 }
 
 export default SearchBar
