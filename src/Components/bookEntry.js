da4ae421567ac0e34393039a7a2a861b0aba1bb7
bhylton <bhylton@stellarstudios.com>
2017-07-03 09:57:05 -0400

    Refactor: Refactor BookEntry to stateless function

--- src/Components/bookEntry.js
+++ src/Components/bookEntry.js
@@ -2,40 +2,33 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-class BookEntry extends Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    onShelfChange: PropTypes.func.isRequired
-  }
-
-  moveBook = (e) => {
-    this.props.onShelfChange(this.props.book, e.target.value);
-  }
-
-  render() {
-    const { book } = this.props
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover"
-            style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-            <select onChange={this.moveBook} value={book.shelf}>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading" >Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+const BookEntry = (props) => {
+  return(
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover"
+          style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
+        <div className="book-shelf-changer">
+          <select onChange={(e) => props.onShelfChange(props.book, e.target.value)} value={props.book.shelf}>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{ book.title }</div>
-        {typeof book.authors !== 'undefined' && (
-          <div className="book-authors">{ book.authors.join(', ') }</div>
-        )}
       </div>
-    )
-  }
+      <div className="book-title">{ props.book.title }</div>
+      {typeof props.book.authors !== 'undefined' && (
+        <div className="book-authors">{ props.book.authors.join(', ') }</div>
+      )}
+    </div>
+  )
+}
+
+BookEntry.propTypes = {
+  book: PropTypes.object.isRequired,
+  onShelfChange: PropTypes.func.isRequired
 }
 
 export default BookEntry
