1f97cd400692f67470908da906d19bcab35be457
bhylton <bhylton@stellarstudios.com>
2017-07-03 10:04:16 -0400

    Refactor: Chang BookShelf to a stateless function

--- src/Components/bookShelf.js
+++ src/Components/bookShelf.js
@@ -6,41 +6,37 @@ import PropTypes from 'prop-types'
 //Child Components
 import BookEntry from './bookEntry'
 
-class BookShelf extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired
-  }
+const BookShelf = (props) => {
+  props.books.sort(sortBy('title'))
 
-  render() {
-    const { title, books, onBookMove } = this.props
-
-    books.sort(sortBy('title'))
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.length > 0 ?
-              books.map((book) => {
-                return (
-                  <li key={book.id}>
-                    <BookEntry
-                      book={book}
-                      onShelfChange={onBookMove}
-                    />
-                  </li>
-                )
-              })
-              :
-              (<span className="no-shelf">No books match this criterion.</span>)
-            }
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.length > 0 ?
+            props.books.map((book) => {
+              return (
+                <li key={book.id}>
+                  <BookEntry
+                    book={book}
+                    onShelfChange={props.onBookMove}
+                  />
+                </li>
+              )
+            })
+            :
+            (<span className="no-shelf">No books match this criterion.</span>)
+          }
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+BookShelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired
 }
 
 export default BookShelf
